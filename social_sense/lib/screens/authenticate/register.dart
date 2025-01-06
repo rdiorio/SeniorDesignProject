df1a3b1ebbeadef4aa5f3477d8426d0bd36627a6
Joshua Phillips <jdphil23462@gmail.com>
2025-01-06 17:11:58 -0500

    full login cosmetics and start of database

--- social_sense/lib/screens/authenticate/register.dart
+++ social_sense/lib/screens/authenticate/register.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:social_sense/services/auth.dart';
+import 'package:social_sense/shared/constants.dart';
+import 'package:social_sense/shared/loading.dart';
 
 class Register extends StatefulWidget {
-  final Function toggleView;  // toggleView is a required parameter
-  Register({required this.toggleView});  // Make sure this is marked as required
+  final Function toggleView; // toggleView is a required parameter
+  Register({required this.toggleView}); // Make sure this is marked as required
 
   @override
   State<Register> createState() => _RegisterState();
 }
 
 class _RegisterState extends State<Register> {
-
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
+  bool loading = false;
 
   // text field state
   String email = '';
@@ -21,70 +23,81 @@ class _RegisterState extends State<Register> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[100],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        title: Text('Sign up to Social Sense'),
-        actions: <Widget>[
-          TextButton.icon(
-            icon: Icon(Icons.person),
-            label: Text('Sign In'),
-            onPressed: () {
-              widget.toggleView();
-            },
-          )
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20.0),
-              TextFormField(
-                validator: (val) => (val?.isEmpty ?? true) ? 'Enter an email' : null,
-                onChanged: (val) {
-                  setState(() => email = val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                obscureText: true,
-                validator: (val) => (val?.length ?? 0) < 6 ? 'Enter a password that is 6 characters long' : null,
-                onChanged: (val) {
-                  setState(() => password = val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.pink[400], // Background color
-                ),
-                child: Text(
-                  'Register',
-                  style: TextStyle(color: Colors.white), // Text color
-                ),
-                onPressed: () async {
-                  if (_formKey.currentState?.validate() ?? false){
-                    dynamic result = await _auth.registerWithEmailAndPassword(email, password);
-                    if(result == null){
-                      setState(() => error = 'please supply a valid email');
-                    }
-                  }
-                },
-              ),
-              SizedBox(height: 12.0),
-              Text(
-                error,
-                style: TextStyle(color: Colors.red, fontSize: 14.0),
-              )
-            ]
-          ),
-        )
-      ),
-    );
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.brown[100],
+            appBar: AppBar(
+              backgroundColor: Colors.brown[400],
+              elevation: 0.0,
+              title: Text('Sign up to Social Sense'),
+              actions: <Widget>[
+                TextButton.icon(
+                  icon: Icon(Icons.person),
+                  label: Text('Sign In'),
+                  onPressed: () {
+                    widget.toggleView();
+                  },
+                )
+              ],
+            ),
+            body: Container(
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(children: <Widget>[
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      decoration:
+                          textInputDecoration.copyWith(hintText: 'Email'),
+                      validator: (val) =>
+                          (val?.isEmpty ?? true) ? 'Enter an email' : null,
+                      onChanged: (val) {
+                        setState(() => email = val);
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      decoration:
+                          textInputDecoration.copyWith(hintText: 'Password'),
+                      obscureText: true,
+                      validator: (val) => (val?.length ?? 0) < 6
+                          ? 'Enter a password that is 6 characters long'
+                          : null,
+                      onChanged: (val) {
+                        setState(() => password = val);
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.pink[400], // Background color
+                      ),
+                      child: Text(
+                        'Register',
+                        style: TextStyle(color: Colors.white), // Text color
+                      ),
+                      onPressed: () async {
+                        if (_formKey.currentState?.validate() ?? false) {
+                          setState(() => loading = true);
+                          dynamic result = await _auth
+                              .registerWithEmailAndPassword(email, password);
+                          if (result == null) {
+                            setState(() {
+                              error = 'please supply a valid email';
+                              loading = false;
+                            });
+                          }
+                        }
+                      },
+                    ),
+                    SizedBox(height: 12.0),
+                    Text(
+                      error,
+                      style: TextStyle(color: Colors.red, fontSize: 14.0),
+                    )
+                  ]),
+                )),
+          );
   }
 }
--- social_sense/lib/screens/authenticate/sign_in.dart
+++ social_sense/lib/screens/authenticate/sign_in.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:social_sense/services/auth.dart';
+import 'package:social_sense/shared/constants.dart';
+import 'package:social_sense/shared/loading.dart';
 
 class SignIn extends StatefulWidget {
-  final Function toggleView;  // toggleView is a required parameter
-  SignIn({required this.toggleView});  // Make sure this is marked as required
+  final Function toggleView; // toggleView is a required parameter
+  SignIn({required this.toggleView}); // Make sure this is marked as required
 
   @override
   State<SignIn> createState() => _SignInState();
 }
 
 class _SignInState extends State<SignIn> {
-
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
-
+  bool loading = false;
 
   // text field state
   String email = '';
@@ -22,69 +23,81 @@ class _SignInState extends State<SignIn> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[100],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        title: Text('Sign in to Social Sense'),
-        actions: <Widget>[
-          TextButton.icon(
-            icon: Icon(Icons.person),
-            label: Text('Register'),
-            onPressed: () {
-              widget.toggleView();
-            },
-          )
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20.0),
-              TextFormField(
-                validator: (val) => (val?.isEmpty ?? true) ? 'Enter an email' : null,
-                onChanged: (val) {
-                  setState(() => email = val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                validator: (val) => (val?.length ?? 0) < 6 ? 'Enter a password that is 6 characters long' : null,
-                onChanged: (val) {
-                  setState(() => password = val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.pink[400], // Background color
-                ),
-                child: Text(
-                  'Sign in',
-                  style: TextStyle(color: Colors.white), // Text color
-                ),
-                onPressed: () async {
-                  if (_formKey.currentState?.validate() ?? false){
-                    dynamic result = await _auth.signInWithEmailAndPassword(email, password);
-                    if(result == null){
-                      setState(() => error = 'COULD NOT SIGN IN WITH THOSE CREDENTIALS');
-                    }
-                  }
-                },
-              ),
-              SizedBox(height: 12.0),
-              Text(
-                error,
-                style: TextStyle(color: Colors.red, fontSize: 14.0),
-              )
-            ]
-          ),
-        )
-      ),
-    );
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.brown[100],
+            appBar: AppBar(
+              backgroundColor: Colors.brown[400],
+              elevation: 0.0,
+              title: Text('Sign in to Social Sense'),
+              actions: <Widget>[
+                TextButton.icon(
+                  icon: Icon(Icons.person),
+                  label: Text('Register'),
+                  onPressed: () {
+                    widget.toggleView();
+                  },
+                )
+              ],
+            ),
+            body: Container(
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(children: <Widget>[
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      decoration:
+                          textInputDecoration.copyWith(hintText: 'Email'),
+                      validator: (val) =>
+                          (val?.isEmpty ?? true) ? 'Enter an email' : null,
+                      onChanged: (val) {
+                        setState(() => email = val);
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      decoration:
+                          textInputDecoration.copyWith(hintText: 'Password'),
+                      validator: (val) => (val?.length ?? 0) < 6
+                          ? 'Enter a password that is 6 characters long'
+                          : null,
+                      onChanged: (val) {
+                        setState(() => password = val);
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.pink[400], // Background color
+                      ),
+                      child: Text(
+                        'Sign in',
+                        style: TextStyle(color: Colors.white), // Text color
+                      ),
+                      onPressed: () async {
+                        if (_formKey.currentState?.validate() ?? false) {
+                          setState(() => loading = true);
+                          dynamic result = await _auth
+                              .signInWithEmailAndPassword(email, password);
+                          if (result == null) {
+                            setState(() {
+                              error =
+                                  'could not sign in with those credentials';
+                              loading = false;
+                            });
+                          }
+                        }
+                      },
+                    ),
+                    SizedBox(height: 12.0),
+                    Text(
+                      error,
+                      style: TextStyle(color: Colors.red, fontSize: 14.0),
+                    )
+                  ]),
+                )),
+          );
   }
 }
